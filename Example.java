a1e56c9c6b8e12f87b7f5e8e862e6fa9bafeda54
sast-qa <123078419+sast-qa@users.noreply.github.com>
2023-06-02 11:47:09 +0200

    Update Example.java

--- Example.java
+++ Example.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Example extends AssignmentEndpoint {
   
+  
  private final LessonDataSource dataSource;  
   
   public Example(LessonDataSource dataSource) {
